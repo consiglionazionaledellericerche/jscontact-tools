611e5f571a1183ed8ed5b18422ba08d12f4bd9b3
mario.loffredo <mario.loffredo@iit.cnr.it>
2021-03-02 16:40:28 +0100

    Cleanup unused import

--- src/test/java/it/cnr/iit/jscontact/tools/test/converters/jscontact2vcard/PreferredContactLanguagesTest.java
+++ src/test/java/it/cnr/iit/jscontact/tools/test/converters/jscontact2vcard/PreferredContactLanguagesTest.java
@@ -16,7 +16,6 @@
 package it.cnr.iit.jscontact.tools.test.converters.jscontact2vcard;
 
 import ezvcard.VCard;
-import it.cnr.iit.jscontact.tools.dto.JSCard;
 import it.cnr.iit.jscontact.tools.exceptions.CardException;
 import org.junit.Test;
 
