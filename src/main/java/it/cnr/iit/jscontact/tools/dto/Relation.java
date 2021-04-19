b628f718e1a7704e7ade0ac9cce9cc268f0a4c7e
mario.loffredo <mario.loffredo@iit.cnr.it>
2021-04-19 10:07:45 +0200

    Added Lombok @Singular annotation to Map<RelationType,String>

--- src/main/java/it/cnr/iit/jscontact/tools/dto/Relation.java
+++ src/main/java/it/cnr/iit/jscontact/tools/dto/Relation.java
@@ -17,10 +17,7 @@ package it.cnr.iit.jscontact.tools.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import it.cnr.iit.jscontact.tools.constraints.BooleanMapConstraint;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.Map;
@@ -33,5 +30,7 @@ import java.util.Map;
 public class Relation extends GroupableObject implements Serializable {
 
     @BooleanMapConstraint(message = "invalid Map<RelationType,Boolean> relation in Relation - Only Boolean.TRUE allowed")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @Singular(value="relationType", ignoreNullCollections = true)
     Map<RelationType,Boolean> relation;
 }
