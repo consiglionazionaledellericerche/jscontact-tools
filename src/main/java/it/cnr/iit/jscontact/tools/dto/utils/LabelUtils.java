63ac0259a13c0a3c96e67da45ab6ab6a61b6848a
mario.loffredo <mario.loffredo@iit.cnr.it>
2021-04-20 15:14:01 +0200

    Code cleanup.

--- src/main/java/it/cnr/iit/jscontact/tools/dto/utils/LabelUtils.java
+++ src/main/java/it/cnr/iit/jscontact/tools/dto/utils/LabelUtils.java
@@ -7,14 +7,26 @@ public class LabelUtils {
 
     public static final String LABEL_DELIMITER = ",";
 
-    public static boolean labelIncludesItem(String label,String labelItem) {
+    public static boolean labelIncludesItem(String label,String item) {
 
         if (label == null)
             return false;
 
         List<String> labelItems = Arrays.asList(label.split(LABEL_DELIMITER));
-        return labelItems.contains(labelItem);
+        return labelItems.contains(item);
 
+    }
+
+    public static boolean labelIncludesAnyItem(String label,List<String> items) {
+
+        if (label == null)
+            return false;
+
+        for (String item : items)
+            if (labelIncludesItem(label, item))
+                return true;
+
+        return false;
     }
 
 }
