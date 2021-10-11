48fa792e158c9dbf3c8b5f8405a903456c0eb33f
mario.loffredo <mario.loffredo@iit.cnr.it>
2021-10-11 16:19:43 +0200

    Added textArrayNode method.

--- src/main/java/it/cnr/iit/jscontact/tools/dto/utils/JsonNodeUtils.java
+++ src/main/java/it/cnr/iit/jscontact/tools/dto/utils/JsonNodeUtils.java
@@ -1,5 +1,6 @@
 package it.cnr.iit.jscontact.tools.dto.utils;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.TextNode;
 
@@ -9,5 +10,12 @@ public class JsonNodeUtils {
 
     public static TextNode textNode(String text) {
         return JSON_NODE_FACTORY.textNode(text);
+    }
+
+    public static ArrayNode textArrayNode(String[] array) {
+        ArrayNode arrayNode = JSON_NODE_FACTORY.arrayNode(array.length);
+        for (int i=0;i < array.length ; i++)
+          arrayNode.set(i, textNode(array[i]));
+        return arrayNode;
     }
 }
