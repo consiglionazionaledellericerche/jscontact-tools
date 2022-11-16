5804038a70dde9c8b873a15d03e48ff820b5fb62
mario.loffredo <mario.loffredo@iit.cnr.it>
2022-11-16 14:39:14 +0100

    Removed type from SchedulingAddress - Again

--- src/main/java/it/cnr/iit/jscontact/tools/dto/SchedulingAddress.java
+++ src/main/java/it/cnr/iit/jscontact/tools/dto/SchedulingAddress.java
@@ -1,11 +1,8 @@
 package it.cnr.iit.jscontact.tools.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import it.cnr.iit.jscontact.tools.dto.interfaces.HasType;
 import it.cnr.iit.jscontact.tools.dto.interfaces.IdMapValue;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -19,7 +16,7 @@ import java.io.Serializable;
  * @see <a href="https://datatracker.ietf.org/doc/draft-ietf-calext-jscontact#section-2.4.2">draft-ietf-calext-jscontact</a>
  * @author Mario Loffredo
  */
-@JsonPropertyOrder({"@type","uri","type","mediaType","contexts","pref","label"})
+@JsonPropertyOrder({"@type","uri","mediaType","contexts","pref","label"})
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @SuperBuilder
 @Data
