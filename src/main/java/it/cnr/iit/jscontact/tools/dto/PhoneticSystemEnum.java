86738c0ac71a4d069cc2c6f83365779beff77d84
mario.loffredo <mario.loffredo@iit.cnr.it>
2023-09-06 08:56:29 +0200

    Changed reference from section 1.5.4 to 1.5.5

--- src/main/java/it/cnr/iit/jscontact/tools/dto/PhoneticSystemEnum.java
+++ src/main/java/it/cnr/iit/jscontact/tools/dto/PhoneticSystemEnum.java
@@ -22,9 +22,9 @@ import it.cnr.iit.jscontact.tools.dto.utils.EnumUtils;
 import lombok.AllArgsConstructor;
 
 /**
- * Enum class mapping the "system" values of the Pronounce type as defined in section 1.5.4 of [draft-ietf-calext-jscontact].
+ * Enum class mapping the "system" values of the Pronounce type as defined in section 1.5.5 of [draft-ietf-calext-jscontact].
  *
- * @see <a href="https://datatracker.ietf.org/doc/draft-ietf-calext-jscontact#section-1.5.4">draft-ietf-calext-jscontact</a>
+ * @see <a href="https://datatracker.ietf.org/doc/draft-ietf-calext-jscontact#section-1.5.5">draft-ietf-calext-jscontact</a>
  * @author Mario Loffredo
  */
 @AllArgsConstructor
