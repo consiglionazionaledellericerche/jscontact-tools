4900a9df7b20e614e43afda006bf93504c8f91f4
mario.loffredo <mario.loffredo@iit.cnr.it>
2023-11-22 15:19:17 +0100

    Fixed constraint on day property in PartialDate

--- src/main/java/it/cnr/iit/jscontact/tools/dto/AnniversaryDate.java
+++ src/main/java/it/cnr/iit/jscontact/tools/dto/AnniversaryDate.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
 import java.io.Serializable;
 
 /**
@@ -22,7 +23,9 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class AnniversaryDate implements Serializable {
 
+    @Valid
     Timestamp date;
+    @Valid
     PartialDate partialDate;
 
     /**
